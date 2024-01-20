8a5b3556d127385881763a0392efc4dd4a9aecc1
Shaditya Kinlekar <shadityakinlekar20@gmail.com>
2024-01-20 12:52:06 +0530

    added sign-in/up .env tailwind-prettier

--- app/(auth)/layout.jsx
+++ app/(auth)/layout.jsx
@@ -1,4 +1,5 @@
 import { Inter } from 'next/font/google';
+import { ClerkProvider } from '@clerk/nextjs'
 import '../globals.css';
 
 export const metadata = {
@@ -10,10 +11,12 @@ const inter = Inter({
   subsets: ['latin'],
 });
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }) { 
   return (
-    <html lang='en'>
-      <body className={`${inter.className} bg-purple-1`}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang='en'>
+        <body className={`${inter.className} bg-purple-1`}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
