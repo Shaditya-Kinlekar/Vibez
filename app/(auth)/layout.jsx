d4ed0ce26214e66838136823d0c8c4b93c79eff1
Shaditya Kinlekar <shadityakinlekar20@gmail.com>
2024-01-20 17:48:06 +0530

    added files to components/

--- app/(auth)/layout.jsx
+++ app/(auth)/layout.jsx
@@ -1,5 +1,5 @@
 import { Inter } from 'next/font/google';
-import { ClerkProvider } from '@clerk/nextjs'
+import { ClerkProvider } from '@clerk/nextjs';
 import '../globals.css';
 
 export const metadata = {
@@ -11,11 +11,11 @@ const inter = Inter({
   subsets: ['latin'],
 });
 
-export default function RootLayout({ children }) { 
+export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang='en'>
-        <body className={`${inter.className} bg-purple-1`}>{children}</body>
+        <body className={`${inter.className} bg-purple-2`}>{children}</body>
       </html>
     </ClerkProvider>
   );
